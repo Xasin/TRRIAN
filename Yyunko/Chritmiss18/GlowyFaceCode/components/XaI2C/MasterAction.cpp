4076ca8fb3c02f268ea4916dbd56a10249e61f80
Xasin <davidbailey.2889@gmail.com>
2018-11-29 15:51:26 +0100

    Fixed wrong write cmd

--- Yyunko/Chritmiss18/GlowyFaceCode/components/XaI2C/MasterAction.cpp
+++ Yyunko/Chritmiss18/GlowyFaceCode/components/XaI2C/MasterAction.cpp
@@ -19,9 +19,10 @@ void MasterAction::init(gpio_num_t sda, gpio_num_t scl, i2c_port_t port) {
 	i2c_cfg.scl_pullup_en = GPIO_PULLUP_ENABLE;
 	i2c_cfg.sda_pullup_en = GPIO_PULLUP_ENABLE;
 
-	i2c_cfg.master.clk_speed = 10;
+	i2c_cfg.master.clk_speed = 100000;
 
 	i2c_param_config(port, &i2c_cfg);
+	i2c_set_period(port, 1000, 1000);
 
 	i2c_driver_install(port, I2C_MODE_MASTER, 0, 0, 0);
 
@@ -44,10 +45,10 @@ void MasterAction::send_start(bool rw) {
 }
 
 void MasterAction::write(uint8_t cCode, void *data, size_t length) {
-	send_start(true);
-	if(data == nullptr)
-		i2c_master_write_byte(cmd, cCode, true);
-	else
+	send_start(false);
+
+	i2c_master_write_byte(cmd, cCode, true);
+	if(data != nullptr)
 		i2c_master_write(cmd, reinterpret_cast<unsigned char *>(data), length, true);
 }
 
@@ -57,7 +58,7 @@ void MasterAction::execute(i2c_port_t port) {
 	i2c_master_stop(cmd);
 
 	executed = true;
-	auto ret = i2c_master_cmd_begin(port, cmd, true);
+	auto ret = i2c_master_cmd_begin(port, cmd, 1000);
 
 	printf("Error-code is: %s\n", esp_err_to_name(ret));
 
